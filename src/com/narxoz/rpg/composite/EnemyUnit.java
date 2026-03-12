43a069b3d31b2b32d3c3948b3a10a2dda140953f
Zarina <zarina@gmail.com>
2026-03-12 23:13:17 +0500

    Added EnemyUnit class Added HeroUnit class

--- src/com/narxoz/rpg/composite/EnemyUnit.java
+++ src/com/narxoz/rpg/composite/EnemyUnit.java
@@ -1,5 +1,4 @@
 package com.narxoz.rpg.composite;
-
 public class EnemyUnit extends UnitLeaf {
     public EnemyUnit(String name, int health, int attackPower) {
         super(name, health, attackPower);
--- src/com/narxoz/rpg/composite/HeroUnit.java
+++ src/com/narxoz/rpg/composite/HeroUnit.java
@@ -1,7 +1,7 @@
 package com.narxoz.rpg.composite;
-
 public class HeroUnit extends UnitLeaf {
     public HeroUnit(String name, int health, int attackPower) {
         super(name, health, attackPower);
+
     }
 }
